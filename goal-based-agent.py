8f0f52102f78d6179550e19433550405a70b916c
johnpauljuma <johnpauljuma29@gmail.com>
2024-05-22 12:06:16 +0300

    added goal-based agent

--- goal-based-agent.py
+++ goal-based-agent.py
@@ -1,4 +1,5 @@
 import random
+import time
 class environment(object):
     def __init__(self):
         self.location = ["A","B"] #locations
@@ -7,27 +8,22 @@ class environment(object):
         self.locationcondition["B"] = random.randint(0,1)
         self.vacuumlocation = random.choice(self.location)
         self.mode = ["T","L"]
-        #Assume past record is A=T and B=T
         self.cleaningmethod = {"A":"T","B":"T"}
-        #Randomly generate intial cleaning history
         self.cleaningmethod["A"] = random.choice (self.mode)
         self.cleaningmethod["B"] = random.choice (self.mode)
 class agent(environment):
     def __init__(self,environment):
-        #Randomly generate status
-        print("Environment condition", environment.locationcondition,
-        "Vacuum location", environment.vacuumlocation, "cleaning method",environment.cleaningmethod)
+        print("Environment condition", environment.locationcondition, "Vacuum location",environment.vacuumlocation, "cleaning method"
+        ,environment.cleaningmethod)
         count = 0
         while count<2:
-            if environment.locationcondition[environment.vacuumlocation]==1:
-                print (environment.vacuumlocation, " is dirty, ")
-                #check status, suck, mark clean, record status
-                if environment.cleaningmethod[environment.vacuumlocation]=="T":
-                    environment.cleaningmethod[environment.vacuumlocation]="L"
+            if environment.locationcondition[environment.vacuumlocation] == 1:
+                if environment.cleaningmethod[environment.vacuumlocation] == "T":
+                    environment.cleaningmethod[environment.vacuumlocation] = "L"
                 else:
-                    environment.cleaningmethod[environment.vacuumlocation]="T"
-                    environment.locationcondition[environment.vacuumlocation]=0
-                    print(environment.vacuumlocation, "Has been cleaned")
+                    environment.cleaningmethod[environment.vacuumlocation] = "T"
+                environment.locationcondition[environment.vacuumlocation] = 0
+                print(environment.vacuumlocation, "Has been cleaned")
             else:
                 print(environment.vacuumlocation, "Is clean")
             newindex = environment.location.index(environment.vacuumlocation)+1
@@ -37,5 +33,11 @@ class agent(environment):
             count += 1
         print("new conditions",environment.locationcondition)
         print("updated history",environment.cleaningmethod)
-e1 = environment()
-a1 = agent(e1)
+#The goal can be achieved follows
+x=0
+while x<24:
+    e1 = environment()
+    a1 = agent(e1)
+    x+=1
+    #Add a 1 hr delay in the loop
+    time.sleep(1)
